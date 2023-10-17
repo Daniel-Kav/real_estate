87c0de7860cf56c0130e5666b42f383a20d29de6
daniel-kav <dankavatha01@gmail.com>
2023-10-17 21:08:29 +0300

    initial template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>
-      danty
-    </h1>
+    <h1 className='text-red-500'>danty</h1>
   )
 }
