e8b05fce2e67dd9326de920b519d608ae5685ca7
daniel-kav <dankavatha01@gmail.com>
2023-10-19 23:15:30 +0300

    routing the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import signIn from './pages/SignIn';
-import signUp from "./pages/signUp";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import About from "./pages/about";
 import Profile from "./pages/Profile";
 
@@ -9,8 +9,8 @@ export default function App() {
   return <BrowserRouter>
   <Routes>
      <Route path="/" element={<Home />} />
-     <Route path="/sign-in" element={<signIn />} />
-     <Route path="/sign-up" element={<signUp />} />
+     <Route path="/sign-in" element={<SignIn />} />
+     <Route path="/sign-up" element={<SignUp />} />
      <Route path="/about" element={<About />} />
      <Route path="/profile" element={<Profile />} />
   </Routes>
--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-export default function signIn() {
+export default function SignIn() {
   return (
     <div>Sign in</div>
-  )
+  );
 }
