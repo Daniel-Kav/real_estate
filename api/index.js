6b608125e8564f5bb33826dd4261d1ffbfd96f02
daniel-kav <dankavatha01@gmail.com>
2023-10-28 00:03:51 +0300

    connected database

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 .then(()=> {
-    console.log('connected to Mongodb server!!!');
+    console.log('connected to Mongodb server');
 })
 .catch(err => console.log(err));
 
