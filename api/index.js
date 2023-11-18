f64b38812d15f1185925329f5d964e8a2fd68f30
daniel-kav <dankavatha01@gmail.com>
2023-11-18 21:58:20 +0300

    connected database

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.listen(3000, ()=> {
-    console.log('server running on port 3000!');
+    console.log('server running on port 3000!!!');
 });
 
 app.use("/api/user", userRouter)
